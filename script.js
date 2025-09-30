32ff1ebb0aa1929b65074c060639b6752caef09b
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-30 10:56:52 +0600

    add 2nd example

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 //==============|Example 01|==========
-async function hello (){
-console.log("hello");
+async function hello() {
+    console.log("hello");
+}
+//==============|Example 02|==========
+
+function Api() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("weather Data");
+            resolve(1200)
+        }, 4000);
+    })
+}
+
+async function getWeatherData() {
+    await Api(); //1st call
+    await Api();  // 2nd call
 
 }
